91af3eb125a73539f4ef7e23dd47c783a9e983b1
Vlad Gorenkin <vladgorenkin@yandex.ru>
2017-12-26 16:28:46 +0600

    zsh: Change leading symbol in minimal theme to ':::'

--- zsh/.zsh/themes/minimal_hack.sh
+++ zsh/.zsh/themes/minimal_hack.sh
@@ -3,13 +3,13 @@ autoload -U colors && colors
 function set-prompt()
 {
   if [[ -z $(git diff --exit-code 2> /dev/null) ]] then
-    vi_mode="%{$fg[grey]%}———"%{$reset_color%}
+    vi_mode="%{$fg[grey]%}:::"%{$reset_color%}
   else
-    vi_mode="%{$fg[green]%}———"%{$reset_color%} 
+    vi_mode="%{$fg[green]%}:::"%{$reset_color%} 
   fi
 
   if [[ $KEYMAP = vicmd ]]; then
-    vi_mode="%{$fg_bold[red]%}———"%{$reset_color%}
+    vi_mode="%{$fg_bold[red]%}:::"%{$reset_color%}
   fi
 
   gitbranch=$(git branch 2> /dev/null | sed -n '/\* /s///p') 
