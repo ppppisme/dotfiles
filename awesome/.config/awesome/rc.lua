2799366892b69baa055ccf72931e90d7439a6112
Vlad Gorenkin <vladgorenkin@yandex.ru>
2018-01-08 14:33:52 +0600

    awesome: move prompt box to the left corner of wibar

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -195,9 +195,9 @@ awful.screen.connect_for_each_screen(function(s)
     layout = wibox.layout.align.horizontal,
     { -- Left widgets
       layout = wibox.layout.fixed.horizontal,
+      s.mypromptbox,
       mylauncher,
       s.mytaglist,
-      s.mypromptbox,
     },
     s.mytasklist, -- Middle widget
     { -- Right widgets
