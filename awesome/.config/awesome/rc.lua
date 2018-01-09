acbf26d9e73a74229c1de5d5427205a08392c5da
Vlad Gorenkin <vladgorenkin@yandex.ru>
2018-01-09 08:35:40 +0600

    awesome: change mappings for screen switch

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -64,15 +64,15 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-  awful.layout.suit.fair,
-  awful.layout.suit.fair.horizontal,
-  awful.layout.suit.floating,
   awful.layout.suit.tile,
   awful.layout.suit.tile.left,
   awful.layout.suit.tile.bottom,
   awful.layout.suit.tile.top,
   awful.layout.suit.spiral,
   awful.layout.suit.spiral.dwindle,
+  awful.layout.suit.fair,
+  awful.layout.suit.fair.horizontal,
+  awful.layout.suit.floating,
   awful.layout.suit.max,
   awful.layout.suit.max.fullscreen,
   awful.layout.suit.magnifier,
@@ -241,20 +241,12 @@ globalkeys = gears.table.join(
     {description = "swap with next client by index", group = "client"}),
   awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
     {description = "swap with previous client by index", group = "client"}),
-  awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
+  awful.key({ modkey,           }, "Tab", function () awful.screen.focus_relative( 1) end,
     {description = "focus the next screen", group = "screen"}),
-  awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
+  awful.key({ modkey, "Shift" }, "Tab", function () awful.screen.focus_relative(-1) end,
     {description = "focus the previous screen", group = "screen"}),
   awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
     {description = "jump to urgent client", group = "client"}),
-  awful.key({ modkey,           }, "Tab",
-    function ()
-      awful.client.focus.history.previous()
-      if client.focus then
-        client.focus:raise()
-      end
-    end,
-    {description = "go back", group = "client"}),
 
   -- Standard program
   awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
