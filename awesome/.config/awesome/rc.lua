7684f6e6a95d56d911178563e32be93d037492b3
Vlad Gorenkin <vladgorenkin@yandex.ru>
2018-01-09 15:18:18 +0600

    awesome: open the last tag ('0') for every screen by default

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -162,6 +162,9 @@ awful.screen.connect_for_each_screen(function(s)
   -- Each screen has its own tag table.
   awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" }, s, awful.layout.layouts[1])
 
+  -- Open the last tag (which should be called '0')
+  awful.tag.viewidx(9, s)
+
   -- Create a promptbox for each screen
   s.mypromptbox = awful.widget.prompt()
   -- Create an imagebox widget which will contain an icon indicating which layout we're using.
