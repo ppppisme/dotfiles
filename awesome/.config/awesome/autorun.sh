5aa9eee9bc1faa6a288fed8f5a2729b29e3245b3
Vlad Gorenkin <vladgorenkin@yandex.ru>
2018-07-07 12:55:02 +0600

    awesome: add unminimize mapping (super + m)

--- awesome/.config/awesome/autorun.sh
+++ awesome/.config/awesome/autorun.sh
@@ -10,4 +10,4 @@ function run {
 run xset m 00 r rate 250 25
 run setxkbmap -model pc104 -layout us,ru -variant qwerty -option grp:alt_shift_toggle -option caps:ctrl_modifier
 run compton
-run redshift -O 5000
+run "redshift -x && redshift -O 5000"
