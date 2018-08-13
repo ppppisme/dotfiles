39b8a1b3c189585b3324fa480ffe30ef7cfc3a15
Vlad Gorenkin <vladgorenkin@yandex.ru>
2018-08-13 22:26:43 +0600

    install: add materia-gtk-theme

--- install.sh
+++ install.sh
@@ -2,17 +2,12 @@
 
 # Applicable for arch linux only.
 
-installer=trizen
+installer="sudo pacman"
 
 $installer -S awesome
 $installer -S neovim
 $installer -S termite
 $installer -S zsh
-
-# Compton with kawase blur effect
-# See https://github.com/tryone144/compton
-# $installer -S compton-tryone-git
-
 
 # vim
 stow vim
@@ -24,3 +19,5 @@ $installer -S pavucontrol
 $installer -S acpi
 $installer -S xclip
 $installer -S feh
+
+$installer -S materia-gtk-theme
