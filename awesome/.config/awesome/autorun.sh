c386781f60506cd2efbdef9cb0083d9e52698d7a
Vlad Gorenkin <vladgorenkin@yandex.ru>
2019-01-05 14:20:21 +0600

    autostart.sh: give redshift a chance to run after nvidia-settings

--- awesome/.config/awesome/autorun.sh
+++ awesome/.config/awesome/autorun.sh
@@ -10,5 +10,5 @@ function run {
 run xset m 00 r rate 250 25
 run setxkbmap -model pc104 -layout us,ru -variant qwerty -option grp:alt_shift_toggle -option caps:ctrl_modifier
 run compton
-run redshift -x && sleep 0.01 && redshift -O 4500
 run nvidia-settings --load-config-only
+run redshift -x && sleep 0.075 && redshift -O 4500
