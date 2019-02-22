d54011ffea9473714e4757358085bed93c0c17b6
Vladislav Gorenkin <vladgorenkin@yandex.ru>
2019-02-22 23:46:09 +0600

    zsh: remove *.rpm highlighting from LS_COLORS

--- zsh/.zsh/variables.sh
+++ zsh/.zsh/variables.sh
@@ -6,5 +6,5 @@ export FFF_COL3=3
 export FFF_COL4=8
 
 # ls
-LS_COLORS='di=1:fi=0:ln=0:pi=0:so=0:bd=0:cd=0:or=0:mi=0:ex=0:*.rpm=0'
+LS_COLORS='di=1:fi=0:ln=0:pi=0:so=0:bd=0:cd=0:or=0:mi=0:ex=0'
 export LS_COLORS
