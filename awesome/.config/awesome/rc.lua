b592e709f270881851ee6b451a52c151cb0ae4cd
Vladislav Gorenkin <vladgorenkin@yandex.ru>
2019-03-08 17:43:36 +0600

    awesome: fix default client rules

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -288,18 +288,20 @@ root.keys(globalkeys) -- luacheck: globals root
 
 -- {{{ Rules
 -- Rules to apply to new clients (through the "manage" signal).
-gears.table.join(awful.rules.rules,
-  { rule = { },
-    properties = {
-      border_width = beautiful.border_width,
-      border_color = beautiful.border_normal,
-      focus = awful.client.focus.filter,
-      raise = true,
-      keys = clientkeys,
-      buttons = clientbuttons,
-      screen = awful.screen.preferred,
-      placement = awful.placement.no_overlap+awful.placement.no_offscreen
-    }
+awful.rules.rules = gears.table.join(awful.rules.rules,
+  {
+    { rule = { },
+      properties = {
+        border_width = beautiful.border_width,
+        border_color = beautiful.border_normal,
+        focus = awful.client.focus.filter,
+        raise = true,
+        keys = clientkeys,
+        buttons = clientbuttons,
+        screen = awful.screen.preferred,
+        placement = awful.placement.no_overlap+awful.placement.no_offscreen
+      }
+    },
   }
 )
 -- }}}
