ab649e3cbfe352eb6c81cb05835d63d7b5ccd765
Vladislav Gorenkin <vladgorenkin@yandex.ru>
2019-03-08 17:33:34 +0600

    awesome: add dump() function to utils library

--- awesome/.config/awesome/utils.lua
+++ awesome/.config/awesome/utils.lua
@@ -1,5 +1,6 @@
 local utils = {}
 local os
+local naughty
 
 -- return execution time of passed callback in msec
 function utils.exec_time(fun)
@@ -11,6 +12,27 @@ function utils.exec_time(fun)
   fun()
 
   return (os.clock() - start) * 1000
+end
+
+function utils.dump(table)
+  if not naughty then
+    naughty = require("naughty")
+  end
+
+  local function dump(t)
+    if type(t) == "table" then
+        local s = "{ "
+        for k, v in pairs(t) do
+          if type(k) ~= "number" then k = '"'..k..'"' end
+          s = s .. "["..k.."] = " .. dump(v) .. ","
+        end
+        return s .. "} "
+    end
+
+    return tostring(t)
+  end
+
+  naughty.notify { text = dump(table), timeout = 0 }
 end
 
 return utils
