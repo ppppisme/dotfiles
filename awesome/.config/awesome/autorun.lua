99d306f54a2e00be2829ae310dcd8b5a74a4617b
scisssssssors <vladgorenkin@yandex.ru>
2020-08-18 23:35:39 +0600

    awesome: update autorun

--- awesome/.config/awesome/autorun.lua
+++ awesome/.config/awesome/autorun.lua
@@ -1,6 +1,8 @@
-local awful = require('awful')
+local awful = require("awful")
 
-awful.spawn.with_shell("xset m 00 r rate 250 25")
-awful.spawn.with_shell("setxkbmap -model pc104 -layout us,ru -variant qwerty -option grp:alt_shift_toggle -option caps:ctrl_modifier")
-awful.spawn.with_shell("redshift -x && redshift -O 4500")
-awful.spawn.with_shell("picom")
+awful.spawn.easy_async_with_shell("xrandr --output DP-0 --right-of HDMI-0 --auto")
+awful.spawn.easy_async_with_shell("xrandr --output HDMI-0 --mode 2560x1080 --rate 144")
+awful.spawn.easy_async_with_shell("xset m 00 r rate 250 25")
+awful.spawn.easy_async_with_shell("setxkbmap -model pc104 -layout us,ru -variant qwerty -option grp:alt_shift_toggle -option caps:ctrl_modifier")
+awful.spawn.easy_async_with_shell("redshift -x && redshift -O 4500")
+awful.spawn.easy_async_with_shell("picom")
